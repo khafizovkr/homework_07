7d9fd6f7e10955ec2776a7f637e4b49b705197e7
razgildai <hafizov-kamil94@yndex.ru>
2021-02-08 18:32:11 +0300

    task_01 and task_03 are done

--- task_01.py
+++ task_01.py
@@ -11,11 +11,21 @@ class Matrix:
         self.matrix = matrix
 
     def __str__(self):
+        m = ''
         for line in self.matrix:
-            print(f'\n')
+            m += '\n'
             for number in line:
-                f'{number}    ', end=''
+                m += str(number) + ' '
+        return m
+
+    def __add__(self, other):
+        result = [[self.matrix[i][j] + other.matrix[i][j] for j in range(len(self.matrix[0]))] for i in range(len(self.matrix))]
+        return Matrix(result)
 
 
-my_matrix = Matrix([[31, 22], [37, 43], [51, 86]])
-str(my_matrix)
+my_matrix1 = Matrix([[31, 22], [37, 43], [51, 86]])
+my_matrix2 = Matrix([[1, 1], [1, 1], [1, 1]])
+sum_matrix = my_matrix1 + my_matrix2
+print(my_matrix1)
+print(my_matrix2)
+print(sum_matrix)
--- task_03.py
+++ task_03.py
@@ -18,3 +18,39 @@
 # строку: *****\n*****\n**.
 # Или, количество ячеек клетки равняется 15, количество ячеек в ряду — 5. Тогда метод make_order() вернет
 # строку: *****\n*****\n*****.
+class Cell:
+    def __init__(self, cells_amount):
+        self.cells_amount = cells_amount
+
+    def __add__(self, other):
+        return self.cells_amount + other.cells_amount
+
+    def __sub__(self, other):
+        if self.cells_amount > other.cells_amount:
+            return self.cells_amount - other.cells_amount
+        else:
+            return 'Результат деления меньше нуля'
+
+    def __mul__(self, other):
+        return self.cells_amount * other.cells_amount
+
+    def __truediv__(self, other):
+        return self.cells_amount // other.cells_amount
+
+    def make_order(self, cells_in_line):
+
+        lines = self.cells_amount // cells_in_line
+        for line in range(lines):
+            print(cells_in_line * '*')
+        print(self.cells_amount % cells_in_line * '*')
+
+
+cell1 = Cell(12)
+cell2 = Cell(7)
+cell1.make_order(3)
+cell2.make_order(5)
+print(cell1 + cell2)
+print(cell1 - cell2)
+print(cell2 - cell1)
+print(cell1 * cell2)
+print(cell1 / cell2)
